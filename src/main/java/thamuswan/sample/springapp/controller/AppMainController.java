b03aaebcf1f2622b54ed236b982abd4101ab7468
Thanawat Muangsawang <thanawat.muangsawang@gmail.com>
2021-04-06 01:05:04 +0700

    Rename the main controller

--- src/main/java/thamuswan/sample/springapp/controller/AppMainController.java
+++ src/main/java/thamuswan/sample/springapp/controller/AppMainController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("main")
-public class MainController {
+public class AppMainController {
 
     @GetMapping("sample-resource")
     public String getSampleResource() {
